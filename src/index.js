e960feef4c45fac5258f0dbe252287e28d4c1732
Fedos-new <fedluky@gmail.com>
2020-05-05 18:45:01 +0700

    refactoring with store

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import * as serviceWorker from './serviceWorker';
-import {state, subscribe} from "./redux/State";
+import {store} from "./redux/State";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./redux/State";
+
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state}
-                 addPost={addPost}
-                 updateNewPostText={updateNewPostText}
-                 addMessage={addMessage}
-                 updateNewMessageText={updateNewMessageText}/>
+            <App state={store.getState()}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}/>
         </BrowserRouter>,
         document.getElementById('root'));
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,74 +1,72 @@
-let rerenderEntireTree = () => {
-    console.log('state changed');
-}
-
-export let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi man! How are you?', likeCount: 5},
-            {id: 2, message: 'My fist post!', likeCount: 9},
-            {id: 3, message: 'Cool!!!', likeCount: 10},
-            {id: 4, message: 'I\'m fine :-)', likeCount: 3},
-            {id: 5, message: 'Where you been?', likeCount: 13},
-            {id: 6, message: 'Learn JS!', likeCount: 19},
-        ],
-        newPostText: ''
+export let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi man! How are you?', likeCount: 5},
+                {id: 2, message: 'My fist post!', likeCount: 9},
+                {id: 3, message: 'Cool!!!', likeCount: 10},
+                {id: 4, message: 'I\'m fine :-)', likeCount: 3},
+                {id: 5, message: 'Where you been?', likeCount: 13},
+                {id: 6, message: 'Learn JS!', likeCount: 19},
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
+                {id: 2, name: 'Roman', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
+                {id: 3, name: 'Leha', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
+                {id: 4, name: 'Ivan', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
+                {id: 5, name: 'Sveta', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you?'},
+                {id: 3, message: 'Yo!'},
+                {id: 4, message: 'WTF?:-)'},
+                {id: 5, message: ':-)'},
+            ],
+            newMessageText: ''
+        },
+        navBar: {},
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
-            {id: 2, name: 'Roman', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
-            {id: 3, name: 'Leha', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
-            {id: 4, name: 'Ivan', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
-            {id: 5, name: 'Sveta', image: 'https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg'},
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Yo!'},
-            {id: 4, message: 'WTF?:-)'},
-            {id: 5, message: ':-)'},
-        ],
-        newMessageText: ''
+    getState() {
+        return this._state
     },
-    navBar: {},
+    _callSubscriber() {
+        console.log('state changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage() {
+        let newMessage = {
+            id: 6,
+            message: this._state.dialogsPage.newMessageText,
+        };
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
 
-};
-
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; //наблюдатель // патерн observer
+    }
 }
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 6,
-        message: state.dialogsPage.newMessageText,
-    };
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
-
-}
-
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export  const subscribe = (observer) =>  {
-    rerenderEntireTree = observer; //наблюдатель // патерн observer
-}
+window.state = store;
