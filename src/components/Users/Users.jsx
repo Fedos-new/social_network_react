5bdb2eff6ad0ea43ac4d1793d385303d923af63c
Fedos-new <fedluky@gmail.com>
2020-06-02 07:56:54 +0700

    replace function component on class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,69 +3,24 @@ import styles from './user.module.css';
 import * as axios from "axios";
 import userPhoto from '../../assets/images/user.png';
 
+class Users extends  React.Component {
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            debugger;
-            props.setUsers(response.data.items)
-        });
-        // [{
-        //         id: 1,
-        //         photoUrl: 'https://avalonuk.com/wp-content/uploads/2015/04/Alex-Horne-27634.jpg',
-        //         followed: false,
-        //         fullName: 'Dmitry Petrov',
-        //         status: 'I am a cool developer',
-        //         location: {city: 'Minsk', country: 'Belarus'}
-        //     },
-        //     {
-        //         id: 2,
-        //         photoUrl: 'https://sefon.pro/img/artist_photos/alex-mica.jpg',
-        //         followed: true,
-        //         fullName: 'Alex Smirnov',
-        //         status: 'I am a cool developer',
-        //         location: {city: 'Moscow', country: 'Russia'}
-        //     },
-        //     {
-        //         id: 4,
-        //         photoUrl: 'https://n1s2.starhit.ru/c2/bd/f8/c2bdf8272296cf755b234c29bf0a2d8a/480x497_0_46e576d5d2ee20acb1b0012b22651833@480x497_0xc0a8399a_6014780351508667136.jpeg',
-        //         followed: true,
-        //         fullName: 'Sveta Bevz',
-        //         status: 'I am a cool developer',
-        //         location: {city: 'Kiev', country: 'Ukrain'}
-        //     },
-        //     {
-        //         id: 5,
-        //         photoUrl: 'https://articlebio.com/uploads/biography/2018/08/01/olga-katysheva.jpg',
-        //         followed: false,
-        //         fullName: 'Olga Sharm',
-        //         status: 'I am a cool developer',
-        //         location: {city: 'Minsk', country: 'Belarus'}
-        //     },
-        //     {
-        //         id: 6,
-        //         photoUrl: 'https://avalonuk.com/wp-content/uploads/2015/04/Alex-Horne-27634.jpg',
-        //         followed: false,
-        //         fullName: 'Igor Ivanov',
-        //         status: 'I am a cool developer',
-        //         location: {city: 'Minsk', country: 'Belarus'}
-        //     },
-        //     {
-        //         id: 7,
-        //         photoUrl: 'https://sefon.pro/img/artist_photos/alex-mica.jpg',
-        //         followed: false,
-        //         fullName: 'Ivan Osipenko',
-        //         status: 'I am a cool developer',
-        //         location: {city: 'Minsk', country: 'Belarus'}
-        //     }]
+    getUsers = () => {
+            if (this.props.users.length === 0) {
+                axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                    debugger;
+                    this.props.setUsers(response.data.items)
+                });
+            }
 
 
-    }
+        }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
@@ -73,15 +28,15 @@ let Users = (props) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
                  </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -91,9 +46,10 @@ let Users = (props) => {
                         <div>{'u.location.city'}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
